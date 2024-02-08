3d229949cf05fa370b36a573bfa23005e3fe2e7f
Ian Raburu <williamraburu@gmail.com>
2024-02-08 10:08:38 +0300

    Fix imports

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const path = require('path')
+import path from 'path';
+import http from 'http';
+import express from 'express';
+import socketio from 'socket.io';
+import formatMessage from './helpers/formatDate';
+import {
+    getActiveUser,
+    exitRoom,
+    newUser,
+    getIndividualRoomUsers
+} from './helpers/userHelper';
 
 // Initialize the app
-const app = express()
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = 3000 || process.env.PORT
 
-// Start the server
-app.listen(PORT, () => console.log(`App is live on port ${PORT}`))
 
+
+
+// Start the server
+app.listen(PORT, () => console.log(`App is live on port ${PORT}`));
