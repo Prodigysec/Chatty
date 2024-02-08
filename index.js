9d06f3f5f4baef7f917aa33ded9b8692c1bcd450
Ian Raburu <williamraburu@gmail.com>
2024-02-08 09:52:51 +0300

    Serve static files from the public directory

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const path = require('path')
 
+// Initialize the app
 const app = express()
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')))
 
 const PORT = 3000 || process.env.PORT
 
-
+// Start the server
 app.listen(PORT, () => console.log(`App is live on port ${PORT}`))
 
